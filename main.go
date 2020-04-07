839575c5f969d243af1a9697140364cad861f439
edwin <spark.com.co@gmail.com>
2020-04-07 18:40:35 -0500

    Switch

--- main.go
+++ main.go
@@ -12,16 +12,28 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	opertion := scanner.Text()
-	values := strings.Split(opertion, "+")
-	operator1, error1 := strconv.Atoi(values[0])
-	operator2, error2 := strconv.Atoi(values[1])
+	operator := "g"
+	values := strings.Split(opertion, operator)
+	value1, error1 := strconv.Atoi(values[0])
+	value2, error2 := strconv.Atoi(values[1])
 	if error1 != nil {
 		fmt.Println(error1)
 	} else {
 		if error2 != nil {
 			fmt.Println(error2)
 		} else {
-			fmt.Println(operator1 + operator2)
+			switch operator {
+			case "+":
+				fmt.Println(value1 + value2)
+			case "-":
+				fmt.Println(value1 - value2)
+			case "*":
+				fmt.Println(value1 * value2)
+			case "/":
+				fmt.Println(value1 / value2)
+			default:
+				fmt.Println("Este operador no está soportado")
+			}
 		}
 	}
 }
