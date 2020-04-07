f46a04e20b0f09a51f7a7da8f416da94018ef6d9
edwin <spark.com.co@gmail.com>
2020-04-07 18:15:01 -0500

    Manejo de errores y uso de if

--- main.go
+++ main.go
@@ -13,7 +13,15 @@ func main() {
 	scanner.Scan()
 	opertion := scanner.Text()
 	values := strings.Split(opertion, "+")
-	operator1, _ := strconv.Atoi(values[0])
-	operator2, _ := strconv.Atoi(values[1])
-	fmt.Println(operator1 + operator2)
+	operator1, error1 := strconv.Atoi(values[0])
+	operator2, error2 := strconv.Atoi(values[1])
+	if error1 != nil {
+		fmt.Println(error1)
+	} else {
+		if error2 != nil {
+			fmt.Println(error2)
+		} else {
+			fmt.Println(operator1 + operator2)
+		}
+	}
 }
