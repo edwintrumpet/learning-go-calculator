2242d5c2a6c217cbc7a12e779248116e5a224de5
edwin <spark.com.co@gmail.com>
2020-04-07 19:35:02 -0500

    Structs y receivers

--- main.go
+++ main.go
@@ -8,32 +8,48 @@ import (
 	"strings"
 )
 
-func main() {
+type calculate struct{}
+
+func (calculate) operate(entry string, operator string) int {
+	value1, value2 := splitEntry(entry, operator)
+	switch operator {
+	case "+":
+		return value1 + value2
+	case "-":
+		return value1 - value2
+	case "*":
+		return value1 * value2
+	case "/":
+		return value1 / value2
+	default:
+		panic("Esta operación no está soportada")
+	}
+}
+
+func parsing(value string) int {
+	intValue, err := strconv.Atoi(value)
+	if err != nil {
+		panic(err)
+	}
+	return intValue
+}
+
+func readEntry() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	opertion := scanner.Text()
-	operator := "g"
-	values := strings.Split(opertion, operator)
-	value1, error1 := strconv.Atoi(values[0])
-	value2, error2 := strconv.Atoi(values[1])
-	if error1 != nil {
-		fmt.Println(error1)
-	} else {
-		if error2 != nil {
-			fmt.Println(error2)
-		} else {
-			switch operator {
-			case "+":
-				fmt.Println(value1 + value2)
-			case "-":
-				fmt.Println(value1 - value2)
-			case "*":
-				fmt.Println(value1 * value2)
-			case "/":
-				fmt.Println(value1 / value2)
-			default:
-				fmt.Println("Este operador no está soportado")
-			}
-		}
-	}
+	return scanner.Text()
+}
+
+func splitEntry(entry string, operator string) (int, int) {
+	values := strings.Split(entry, operator)
+	value1 := parsing(values[0])
+	value2 := parsing(values[1])
+	return value1, value2
+}
+
+func main() {
+	entry := readEntry()
+	operator := readEntry()
+	calculateInstance := calculate{}
+	fmt.Println(calculateInstance.operate(entry, operator))
 }
